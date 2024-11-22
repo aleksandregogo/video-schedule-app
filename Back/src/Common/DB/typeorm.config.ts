9846dee9edfd45d55f6401bf9015985a04e83786
aleksandre <agogotishvili@baasi.com>
2024-11-22 16:23:04 +0400

    db fix

--- Back/src/Common/DB/typeorm.config.ts
+++ Back/src/Common/DB/typeorm.config.ts
@@ -2,6 +2,8 @@ import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { APP_CONFIG, AppConfig } from '../Config/app.config';
 import { User } from 'src/Entities/user.entity';
+import { Company } from 'src/Entities/company.entity';
+import { Location } from 'src/Entities/location.entity';
 
 export const TypeormAsyncConfig: TypeOrmModuleAsyncOptions = {
   inject: [APP_CONFIG],
@@ -15,7 +17,7 @@ export const TypeormAsyncConfig: TypeOrmModuleAsyncOptions = {
       username: appConfig.db.username,
       password: appConfig.db.password,
       database: appConfig.db.database,
-      entities: [User],
+      entities: [User, Location, Company],
       migrationsRun: false,
       synchronize: false,
       logging: true,
